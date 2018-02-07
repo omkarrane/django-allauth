736c44175cf746f2f340074e4948befe9164e9dc
omkarrane <omkarrane10@gmail.com>
2018-02-08 04:31:33 +0530

    final

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -135,19 +135,3 @@ AUTHENTICATION_BACKENDS = [ 'django.contrib.auth.backends.ModelBackend',
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = "/profile/"
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = {'HTTP_X_FORWARDED_PROTO', 'https'}
-
-ALLOWED_HOSTS = ['*']
-
-import os
-BASE_DIR = os.path.dirname{os.path.abspath(__file__)}
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = {
-    os.path.join(BASE_DIR, 'static'),
-}
